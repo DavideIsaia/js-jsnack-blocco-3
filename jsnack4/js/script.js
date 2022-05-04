443fa81891336b1364198718e718dab74b99e87f
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-04 16:28:12 +0200

    aggiunta visualizzazione array su dom

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,1 +1,19 @@
 // Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+let $ = document.querySelector.bind(document);
+
+// creo un array vuoto
+const numbArray = [];
+
+// riempio l'array con numeri casuali
+do {
+    numbArray.push(randomNumber1to100());     
+} while (numbArray.length < 10);
+
+console.log(numbArray);
+$('#array').innerHTML = numbArray;
+
+// la funzione genera un numero da 1 a 100 e lo dà come output.
+function randomNumber1to100 () {
+    return Math.floor(Math.random() *100) +1;
+}
